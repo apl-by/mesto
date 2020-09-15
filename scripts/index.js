1666a3174dd893af7e03bec40fe60b9f11841cfc
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2020-09-15 22:14:12 +0300

    refactor:change let to const

--- scripts/index.js
+++ scripts/index.js
@@ -1,70 +1,64 @@
-let popup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__edit-button');
-let popupCloseButton = popup.querySelector('.popup__close');
-let formElement = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('input[name="name"]');
-let jobInput = popup.querySelector('input[name="job"]');
-let profileName = profile.querySelector('.profile__name');
-let profileJob = profile.querySelector('.profile__job');
+const popup = document.querySelector('.popup');
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const popupCloseButton = popup.querySelector('.popup__close');
+const formElement = popup.querySelector('.popup__container');
+const nameInput = popup.querySelector('input[name="name"]');
+const jobInput = popup.querySelector('input[name="job"]');
+const profileName = profile.querySelector('.profile__name');
+const profileJob = profile.querySelector('.profile__job');
 const cardsList = document.querySelector('.cards__list');
 const cardTemplate = document.querySelector('.js-card').content;
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-      alt: 'Архыз'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-      alt: 'Природа челябинской области'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-      alt: 'Спальный район Иваново'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-      alt: 'Подножие горы'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-      alt: 'Железная дорога'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-      alt: 'Зимний Байкал'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
 const renderInitialCards = () => {
-  initialCards.forEach(element => {
-    const  copyCardTemplate = cardTemplate.cloneNode(true);
-    copyCardTemplate.querySelector('.card__title').textContent = element.name;
-    copyCardTemplate.querySelector('.card__img').src = element.link;
-    copyCardTemplate.querySelector('.card__img').alt = element.alt;
+  initialCards.forEach(elt => {
+    const copyCardTemplate = cardTemplate.cloneNode(true);
+    copyCardTemplate.querySelector('.card__title').textContent = elt.name;
+    copyCardTemplate.querySelector('.card__img').src = elt.link;
+    copyCardTemplate.querySelector('.card__img').alt = elt.name;
     cardsList.append(copyCardTemplate);
   })
 }
 
-let popupOpen = () => {
+const popupOpen = () => {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-let popupClose = (ev) => {
+const popupClose = (ev) => {
   if (ev.target === ev.currentTarget) {
     popup.classList.remove('popup_opened');
   }
 }
 
-let formSubmitHandler = (ev) => {
+const formSubmitHandler = (ev) => {
   ev.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
