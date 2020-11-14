07c755f6f4dbffc5109c1d3bb44f019823dfb682
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2020-11-14 12:23:01 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,69 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: { importLoaders: 1 }
-            },
-              'postcss-loader'
-            ],
-        },
-        {
-          test: /\.js$/,
-          exclude: '/node_modules/',
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
+  entry: { main: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
-        },
-        {
-          test: /\.(png|svg|jpg|gif|woff2|woff)$/,
-          loader: 'file-loader'
-        },
-        {
-          test: /\.html$/,
-          loader: 'html-loader',
-        },
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      }),
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif|)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './images/',
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(woff2|woff)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './vendor/',
+            },
+          },
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
 
-      new MiniCssExtractPlugin()
-    ],
-    
+    new MiniCssExtractPlugin()
+  ],
+
 }
